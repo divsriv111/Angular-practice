2a4a65228ea04ae7c5c4696c2e7cd32e2ed33aa1
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2022-09-22 13:37:55 +0530

    added componetns

--- customer-app/src/app/app.module.ts
+++ customer-app/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routing/app-routing.module';
+import { AppComponent } from './home/app.component';
 import { SupplierComponent } from './supplier/supplier.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SupplierComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SupplierComponent, CustomerComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
